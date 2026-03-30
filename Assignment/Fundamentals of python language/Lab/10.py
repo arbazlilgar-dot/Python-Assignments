3c903e8f674e99bd6da8b91f73d86829017202f5
arbazlilgar-dot <arbazlilgar@gmail.com>
2026-03-30 09:07:28 +0530

    hello

--- Assignment/Fundamentals of python language/Lab/10.py
+++ Assignment/Fundamentals of python language/Lab/10.py
@@ -1,18 +1,12 @@
 # Practical Example 6: Write a Python program to check if a number is prime using if_else.
 
-num = 7
-prime = True
-
-# checking prime condition by dividing number
+num = int(input("Enter a number: "))
 if num > 1:
     for i in range(2, num):
         if (num % i) == 0:
-            prime = False
+            print("Not a Prime Number")
             break
-            
-    if prime:
-        print(num, "is a prime number")
-    else:
-        print(num, "is not a prime number")
+        else:
+         print("Prime Number")
 else:
-    print(num, "is not a prime number")
+    print("Not a Prime Number")
